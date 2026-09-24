deee4b8ee73bf6d00349a83f32f28ebf3db49a1b
aslam-nexonetics <aslam-nexonetics@users.noreply.github.com>
2026-09-24 10:57:14 +0000

    deploy: a32ab6410f267e16d60f136b15feaec87aa7d64e

---
+++ previews/next-feature-authentication/_next/static/SHNlaKgrMPZjY9AqwZgHL/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Tennis-Players-App/previews/next-feature-authentication//_next/:path+",
+        "destination": "/Tennis-Players-App/previews/next-feature-authentication/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
